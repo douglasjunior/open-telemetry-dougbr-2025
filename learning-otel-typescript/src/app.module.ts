1aeb2ba1c604bba93f50b4cda9fc0e4bb4eebc45
Douglas Junior <nassifrroma@gmail.com>
2025-07-07 19:18:02 -0300

    Init

---
+++ learning-otel-typescript/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+import * as path from 'node:path';
+
+import { TodoModule } from './todo/todo.module';
+import { AppConfigModule } from './app-config/app-config.module';
+import { AppConfigService } from './app-config/app-config.service';
+
+@Module({
+  imports: [
+    LoggerModule.forRoot(),
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      inject: [AppConfigService],
+      useFactory: (appConfigService: AppConfigService) => {
+        return {
+          type: 'sqlite',
+          database: path.join('database', 'db.sqlite'),
+          entities: [path.join(__dirname, '**', '*.model.{ts,js}')],
+          synchronize: appConfigService.databaseSynchronize,
+        }
+      },
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
---
+++ learning-otel-typescript/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
+
+import { AppModule } from './app.module';
+import { AppConfigService } from './app-config/app-config.service';
+import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception.filter';
+
+const config = new DocumentBuilder()
+  .setTitle('Todo example')
+  .setVersion('1.0')
+  .build();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  const appConfigService = app.get(AppConfigService);
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  await app.listen(appConfigService.serverPort);
+  console.log('Server started on port: ' + appConfigService.serverPort)
+}
+
+bootstrap().catch(console.error);
